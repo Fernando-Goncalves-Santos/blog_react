5c9ec83a624ddf79446d12e97a9ac993a8ca83d5
Fernando Goncalves <fernando_goncalves_santos@hotmail.com.br>
2024-10-25 17:18:17 -0300

    Estilizando a Home

--- blog_react/src/routes/Home.jsx
+++ blog_react/src/routes/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
     }, [])
 
   return (
-    <div>
+    <div className='home'>
         <h1>Últimos Posts</h1>
         {posts.length === 0 ? (<p>Carregando...</p>) : (
             posts.map((post) => (
