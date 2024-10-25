f1da87c8942bac02bc84a9e70ac17fa87e58228e
Fernando Goncalves <fernando_goncalves_santos@hotmail.com.br>
2024-10-25 16:39:40 -0300

    Configurando o Rounter

--- blog_react/src/App.jsx
+++ blog_react/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
-
+import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
     <>
         <h1>React Axios</h1>
+        <div className="container">
+          <Outlet/>
+        </div>
         
     </>
   )
