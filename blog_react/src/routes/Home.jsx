71dd2f5c0cd0ccd0b88f3ad9839d465440e5197a
Fernando Goncalves <fernando_goncalves_santos@hotmail.com.br>
2024-10-25 17:12:08 -0300

    Exibindo os dados da API na Home

--- blog_react/src/routes/Home.jsx
+++ blog_react/src/routes/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
             const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
             const data = response.data
-            console.log(data)
+            setPosts(data)
 
 
         } catch (error) {
@@ -32,7 +32,18 @@ const Home = () => {
     }, [])
 
   return (
-    <div>Home</div>
+    <div>
+        <h1>Últimos Posts</h1>
+        {posts.length === 0 ? (<p>Carregando...</p>) : (
+            posts.map((post) => (
+                <div className="post" key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+                </div>
+            ))
+        )}
+    </div>
   )
 }
 
