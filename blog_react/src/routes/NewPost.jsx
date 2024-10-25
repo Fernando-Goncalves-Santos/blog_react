f1ba523565e2aba160889321f0f86ee90f5c22f1
Fernando Goncalves <fernando_goncalves_santos@hotmail.com.br>
2024-10-25 17:53:02 -0300

    Criando o metodo POST

--- blog_react/src/routes/NewPost.jsx
+++ blog_react/src/routes/NewPost.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import "./NewPost.css"
+import blogFetch from "../axios/config";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NewPost = () => {
+    
+    const navigate = useNavigate()
+    const [title, setTitle] = useState()
+    const [body, setBody] = useState()
+
+    const createPost = async (e) => {
+        e.preventDefault()
+
+        const post = {title, body, userId: 1}
+
+        await blogFetch.post("/posts", {
+            body: post,
+        })
+
+        navigate("/")
+
+    }
+
+
+
   return (
     <div className="new-post">
       <h2>Inserir novo Post:</h2>
-      <form>
+      <form onSubmit={(e) => createPost(e)}>
         <div className="form-control">
           <label htmlFor="title">Título:</label>
-          <input
+          <input onChange={(e) => setTitle(e.target.value)}
             type="text"
             name="title"
             id="title"
@@ -17,7 +40,7 @@ const NewPost = () => {
         </div>
         <div className="form-control">
           <label htmlFor="body">Conteúdo:</label>
-          <textarea
+          <textarea onChange={(e) => setBody(e.target.value)}
             name="body"
             id="body"
             placeholder="Digite o conteúdo"
